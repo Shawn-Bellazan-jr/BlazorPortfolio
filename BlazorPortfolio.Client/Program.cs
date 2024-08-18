4e6b81f22fc9a5128209f7c78815029b591d34ec
Shawn-Bellazan-jr <shawn_bellazan@outlook.com>
2024-08-17 23:35:32 -0500

    feat(BlazorPortfolio.Client): Integrate PortfolioService and display project data
    Integrated the PortfolioService into the BlazorPortfolio.Client application. This involved injecting the service into the Home component and using it to fetch and display the portfolio data.
    Updated the Home component to retrieve the portfolio data from the PortfolioService and render the ProjectCard components for each project in the portfolio.
    Added necessary using statements in _Imports.razor and registered the PortfolioService in Program.cs to make it accessible throughout the application.

--- BlazorPortfolio.Client/Program.cs
+++ BlazorPortfolio.Client/Program.cs
@@ -1,4 +1,5 @@
 using BlazorPortfolio.Client;
+using BlazorPortfolio.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -10,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMudServices();
+builder.Services.AddScoped<PortfolioService>();
 
 await builder.Build().RunAsync();
