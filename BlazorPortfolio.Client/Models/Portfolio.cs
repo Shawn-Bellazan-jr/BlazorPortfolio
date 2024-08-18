6d17b84046695b2012a78edc9d0d279b9eff6176
Shawn-Bellazan-jr <shawn_bellazan@outlook.com>
2024-08-17 23:29:52 -0500

    feat(BlazorPortfolio.Client): Create project and portfolio models
    Created C# models for representing project and portfolio data within the BlazorPortfolio.Client application. These models define the structure and properties of the data that will be used to display project information.
    The Project model includes properties such as title, description, technologies used, key features, challenges faced, solutions implemented, screenshots, demo link, and repository link.
    The Portfolio model includes a list of Project objects to represent a collection of projects.

--- BlazorPortfolio.Client/Models/Portfolio.cs
+++ BlazorPortfolio.Client/Models/Portfolio.cs
@@ -1,7 +1,10 @@
-﻿namespace BlazorPortfolio.Client.Models
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorPortfolio.Client.Models
 {
     public class Portfolio
     {
-        public List<Project> Projects { get; set; }
+        [JsonPropertyName("projects")]
+        public List<Project> Projects { get; set; } = new();
     }
 }
--- BlazorPortfolio.Client/Models/Project.cs
+++ BlazorPortfolio.Client/Models/Project.cs
@@ -1,15 +1,34 @@
-﻿namespace BlazorPortfolio.Client.Models
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorPortfolio.Client.Models
 {
     public class Project
     {
+        [JsonPropertyName("title")]
         public string Title { get; set; }
+
+        [JsonPropertyName("description")]
         public string Description { get; set; }
+
+        [JsonPropertyName("technologies")]
         public List<string> Technologies { get; set; }
+
+        [JsonPropertyName("keyFeatures")]
         public List<string> KeyFeatures { get; set; }
+
+        [JsonPropertyName("challenges")]
         public string Challenges { get; set; }
+
+        [JsonPropertyName("solutions")]
         public string Solutions { get; set; }
+
+        [JsonPropertyName("screenshots")]
         public List<string> Screenshots { get; set; }
+
+        [JsonPropertyName("demoLink")]
         public string DemoLink { get; set; }
+
+        [JsonPropertyName("repositoryLink")]
         public string RepositoryLink { get; set; }
     }
 }
